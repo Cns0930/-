5bebb41fd0b1c590662d437166f0c7f4d08ab3cf
chime <yepeixuan@seassoon.com>
2021-11-26 16:58:27 +0800

    提取:实现合并结构化数据

--- src/main/java/com/seassoon/bizflow/flow/BizFlow.java
+++ src/main/java/com/seassoon/bizflow/flow/BizFlow.java
@@ -8,8 +8,7 @@ import com.seassoon.bizflow.core.model.Output;
 import com.seassoon.bizflow.core.model.config.CheckpointConfig;
 import com.seassoon.bizflow.core.model.config.RuleConfig;
 import com.seassoon.bizflow.core.model.config.SortConfig;
-import com.seassoon.bizflow.core.model.extra.DocumentKV;
-import com.seassoon.bizflow.core.model.extra.ExtraKVInfo;
+import com.seassoon.bizflow.core.model.extra.*;
 import com.seassoon.bizflow.core.model.ocr.Image;
 import com.seassoon.bizflow.core.model.Input;
 import com.seassoon.bizflow.core.model.ocr.OcrOutput;
@@ -24,15 +23,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -143,8 +141,141 @@ public class BizFlow extends AbstractFlow {
         return null;
     }
 
+    /**
+     * 合并结构化数据
+     *
+     * @param docKVs 提取结果
+     * @param extraKVs 结构化数据
+     * @param images 图片信息
+     * @param checkpoints 提取配置
+     */
     private void mergeExtraKV(List<DocumentKV> docKVs, List<ExtraKVInfo> extraKVs, List<Image> images, List<CheckpointConfig> checkpoints) {
-        // TODO...
+        // 没有结构化数据  没有提取信息  跳过
+        if(CollectionUtils.isEmpty(extraKVs) || CollectionUtils.isEmpty(docKVs)){
+            return;
+        }
+        // 只取第一个（这是什么操作?）
+        ExtraKVInfo extraKVInfo = extraKVs.get(0);
+        // source不为超级帮办跳过  没有内容跳过
+        if(Objects.isNull(extraKVInfo) || !"cjbb".equals(extraKVInfo.getSource()) ||
+                CollectionUtils.isEmpty(extraKVInfo.getDocumentList())){
+            return;
+        }
+
+        // 后面需要多次查找的数据先处理成map
+        // documentLabel -> image
+        Map<String, List<Image>> imageMap = new HashMap<>();
+        images.forEach( image -> {
+            List<Image> list = imageMap.getOrDefault(image.getDocumentLabel(), new ArrayList<>());
+            list.add(image);
+            imageMap.put(image.getDocumentLabel(), list);
+        });
+        // documentLabel -> documentField -> fieldKV
+        Map<String, Map<String, FieldKV>> kvMap = new HashMap<>();
+        extraKVInfo.getDocumentList().forEach( document -> {
+            Map<String, FieldKV> map = kvMap.getOrDefault(document.getDocumentLabel(), new HashMap<>());
+            document.getFieldVal().forEach( fieldKV -> {
+                map.putIfAbsent(fieldKV.getKey(), fieldKV);
+            });
+            kvMap.put(document.getDocumentLabel(), map);
+        });
+        // documentLabel -> checkpoint
+        Map<String, CheckpointConfig> checkMap = new HashMap<>();
+        checkpoints.forEach( checkpointConfig -> {
+            checkMap.putIfAbsent(checkpointConfig.getFormTypeId(), checkpointConfig);
+        });
+
+        // 对提取结果遍历进行替换操作
+        docKVs.forEach( documentKV -> {
+            String documentLabel = documentKV.getDocumentLabel();
+            List<Image> imageList = imageMap.getOrDefault(documentLabel, new ArrayList<>());
+            Map<String, FieldKV> kv = kvMap.get(documentLabel);
+            CheckpointConfig checkpointConfig = checkMap.get(documentLabel);
+            List<Content> contentList = documentKV.getDocumentList();
+            contentList.forEach( content -> {
+                String documentField = content.getDocumentField();
+                FieldKV fieldKV = kv.get(documentField);
+
+                // 填入值
+                if(fieldKV != null && !CollectionUtils.isEmpty(fieldKV.getVal())) {
+                    // 如果匹配到
+                    content.setSource(extraKVInfo.getSource());
+                    // 如果有提取结果  替换为一个  取提取的第一个的坐标 和结构化的值
+                    if(CollectionUtils.isEmpty(content.getValueInfo())){
+                        content.setValueInfo(Arrays.asList(Field.of(fieldKV.getVal().get(0), calLocation(content.getValueInfo()), 1.0)));
+                    }else{
+                        // 如果没有提取结果  新增一条
+                        content.setValueInfo(Arrays.asList(Field.of(fieldKV.getVal().get(0), null,1.0)));
+                    }
+                }
+
+                // 填入image_id 和 document_page
+                if(!StringUtils.hasLength(content.getImageId()) && !CollectionUtils.isEmpty(imageList)){
+                    // 筛选document_source
+                    List<Image> filterList = imageList.stream().filter( image ->
+                        image.getDocumentSource() != null && image.getDocumentSource() == 1
+                    ).collect(Collectors.toList());
+                    if(!CollectionUtils.isEmpty(filterList)) {
+                        // 默认情况 取第一个
+                        content.setImageId(filterList.get(0).getImageId());
+                        content.setPage(filterList.get(0).getDocumentPage());
+                        if (checkpointConfig != null && checkpointConfig.getMultiPage()) {
+                            // 多页情况 找到提取点 得到页码
+                            Optional<CheckpointConfig.ExtractPoint> optional = checkpointConfig.getExtractPoint().stream()
+                                    .filter(extractPoint -> documentField.equals(extractPoint.getDocumentField())).findFirst();
+                            if (optional.isPresent()) {
+                                CheckpointConfig.ExtractPoint extractPoint = optional.get();
+                                for (Image image : imageList) {
+                                    // 根据页码 找到正确的image
+                                    if(image.getDocumentPage().equals(extractPoint.getPage())){
+                                        content.setImageId(image.getImageId());
+                                        content.setPage(image.getDocumentPage());
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // 其他字段填充
+                if(!StringUtils.hasLength(content.getIsTrue())){
+                    // 字符串 true 是个什么操作
+                    content.setIsTrue("true");
+                }
+                if(content.getPage() == null){
+                    content.setPage(-1);
+                }
+
+            });
+        });
+
+    }
+
+    /**
+     * 合并结构化数据 - 合并坐标计算
+     * @param valueInfo
+     * @return
+     */
+    private List<List<String>> calLocation(List<Field> valueInfo){
+        if(valueInfo.size() == 1){
+            return valueInfo.get(0).getFieldLocation();
+        }
+        List<List<String>> location = new ArrayList<>();
+        for(Field field : valueInfo){
+            if(field.getFieldLocation() != null && field.getFieldLocation().size() == 2){
+                if(location.isEmpty()){
+                    location.add(field.getFieldLocation().get(0));
+                }else if(location.size() == 1){
+                    location.add(field.getFieldLocation().get(1));
+                }else {
+                    // 替换第二个元素
+                    location.remove(1);
+                    location.add(field.getFieldLocation().get(1));
+                }
+            }
+        }
+        return location;
     }
 
     @Override
